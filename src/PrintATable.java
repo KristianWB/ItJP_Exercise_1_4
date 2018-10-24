1e101964b62e46d6823b679014b12256b17c292f
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-10-24 14:06:45 +0200

    Initial commit

---
+++ src/PrintATable.java
@@ -0,0 +1,7 @@
+public class PrintATable {
+    public static void main(String[] args)   {
+        System.out.print(
+                "a     a^2    a^3    a^4\n1     1      1      1\n2     4      8      16\n3     9      27     81 \n"+
+                        "4     16     64     256");
+            }
+}
